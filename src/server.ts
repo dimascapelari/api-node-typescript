cc85f1e4d956235d95ac14801c031323279ba006
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2026-03-18 15:20:53 -0300

    Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import type { Request, Response, NextFunction } from "express";
+import cors from "cors";
 import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
+import path from "path";
+import swaggerDocument from "../swagger.json";
 import { router } from "./routes";
 
 const app = express();
 const port = 3333;
 app.use(express.json());
+app.use(cors());
 app.use(router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
@@ -21,6 +28,12 @@ app.use(
     });
   },
 );
+
+app.get("/terms", (request: Request, response: Response) => {
+  return response.json({
+    message: "Termos de serviço",
+  });
+});
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
